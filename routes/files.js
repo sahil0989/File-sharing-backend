93c4bfeb56f95faea37ab257850b3d5f945a2b83
jarvis <sahilbarak737@gmail.com>
2023-04-23 09:03:09 +0530

    changes

--- routes/files.js
+++ routes/files.js
@@ -30,42 +30,4 @@ router.post('/', (req, res) => {
       });
 });
 
-router.post('/send', async (req, res) => {
-  const { uuid, emailTo, emailFrom, expiresIn } = req.body;
-  if(!uuid || !emailTo || !emailFrom) {
-      return res.status(422).send({ error: 'All fields are required except expiry.'});
-  }
-  // Get data from db 
-  try {
-    const file = await File.findOne({ uuid: uuid });
-    if(file.sender) {
-      return res.status(422).send({ error: 'Email already sent once.'});
-    }
-    file.sender = emailFrom;
-    file.receiver = emailTo;
-    const response = await file.save();
-    // send mail
-    const sendMail = require('../services/mailService');
-    sendMail({
-      from: emailFrom,
-      to: emailTo,
-      subject: 'inShare file sharing',
-      text: `${emailFrom} shared a file with you.`,
-      html: require('../services/emailTemplate')({
-                emailFrom, 
-                downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email` ,
-                size: parseInt(file.size/1000) + ' KB',
-                expires: '24 hours'
-            })
-    }).then(() => {
-      return res.json({success: true});
-    }).catch(err => {
-      return res.status(500).json({error: 'Error in email sending.'});
-    });
-} catch(err) {
-  return res.status(500).send({ error: 'Something went wrong.'});
-}
-
-});
-
 module.exports = router;
